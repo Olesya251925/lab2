f50c7145f7fa28b05e83f025524c97da07241d46
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-02-19 18:30:55 +0700

    Update n.cpp

--- PDF/n.cpp
+++ PDF/n.cpp
@@ -1,1 +1,76 @@
+/**************************
+* Лабораторная работа № 2 *
+*        MS Word          *
+***************************/
+using System;
+using System.Reflection.Emit;
 
+namespace ConsoleApp{
+  class PDF {
+	public string DateCreation { get; set; }
+    public string DateChange { get; set; }
+    public string Size { get; set; }
+    public string Password { get; set; }
+    public PDF() { }
+
+	public PDF(
+	  string Name,
+      string Author,
+      string [] Keywords,
+      string Subjects,
+      string Path,
+      string DateCreation,
+      string DateChange,
+      string Size,
+      string Password
+    ) : base(Name, Author, Keywords, Subjects, Path){
+          this.DateOfCreation = DateOfCreation;
+          this.DateOfChange = DateOfChange;
+          this.Size = Size;
+          this.Password = Password;
+        }
+		
+		public PDF(string Name) : base(Name) { }
+        public override void PrintValues() {
+		  string ParameterValue = "";
+
+		  Console.Write("Тип файла: ");
+		  Console.WriteLine("PDF");
+          base.ValueParameter();
+
+		if (this.DateCreation == "") {
+            ValueParameter = "Не существует";
+        } else {
+            ValueParameter = this.DateCreation;
+          }
+
+          base.Print("Дата создания: ", ValueParameter);
+          ValueParameter = "";
+
+          if (this.DateChange == "") {
+            ValueParameter = "Не существует";
+          } else{
+            ValueParameter = this.DateChange;
+          }
+
+          base.Print("Дата изменения: ", ValueParameter);
+          ValueParameter = "";
+
+          if (this.Size == "") {
+            ParameterValue = "Не существует";
+          } else {
+              ParameterValue = this.Size;
+            }
+            
+            base.Print("Размер файла: ", ParameterValue);
+            ParameterValue = "";
+
+            if (this.Password == "") {
+              ParameterValue = "Не существует";
+            } else {
+              ParameterValue = this.Password;
+            }
+            base.Print("Введите пароль: ", ParameterValue);
+        }
+    }
+}
