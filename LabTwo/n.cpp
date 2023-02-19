a053fd4cb5579902e8fe8fae3f9f52cf0e12b4ae
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-02-19 19:08:23 +0700

    Create n.cpp

---
+++ LabTwo/n.cpp
@@ -0,0 +1,61 @@
+/**************************
+* Лабораторная работа № 2 *
+***************************/
+using System;
+using System.Reflection.Emit;
+
+namespace ConsoleApp{
+  class LabTwo {
+	public string Name { get; set; }
+	public string Author { get; set; }
+	public string [] Keywords { get; set; }
+	public string Theme { get; set; }
+	public string Path { get; set; }
+
+	public LabTwo(string Name, string Author, string[] Keywords, string Theme, string Path){
+	  this.SetValues(Name, Author, Keywords, Theme, Path);
+	}
+
+	public LabTwo(string Name) => this.Name = Name;
+    public LabTwo () { }
+
+	public void SetValues(
+	  string Name,
+	  string Author,
+	  string [] Keywords,
+	  string Subjects,
+	  string Path
+	) {
+		this.Name = Name;
+		this.Author = Author;
+		this.Keywords = Keywords;
+		this.Subjects = Subjects;
+		this.Path = Path;
+	  }
+
+	  protected virtual void Print(string Parameter, string ParameterValue) {
+	    Console.Write(Parameter);
+		Console.ForegroundColor = ConsoleColor.DarkGreen;
+		Console.WriteLine(ValueParameter);
+	  }
+
+	  public virtual void PrintValues() {
+	    string ValueParameter = "";
+
+		foreach (string el in Keywords) {
+		  if (ValueParameter == "") {
+		    ValueParameter = el;
+		  } else {
+			  ValueParameter += ", " + el;
+			}
+		}
+
+		Print("Имя: ", Name);
+		Print("Автор: ", Author);
+		Print("Ключевые слова: ", ValueParameter);
+		Print("Тематика: ", Subjects);
+		Print("Путь к файлу: ", Path);
+	    ValueParameter = "";
+        }
+	}
+}
