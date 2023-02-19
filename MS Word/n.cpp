eb7236e16203dc13f9f70ceae421da924fb69b7b
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-02-19 23:24:32 +0700

    Update n.cpp

--- MS Word/n.cpp
+++ MS Word/n.cpp
@@ -7,9 +7,6 @@ using System.Reflection.Emit;
 
 namespace ConsoleApp{
   class MS Word : LabTwo {
-    public string DateCreation { get; set; }
-    public string DateChange { get; set; }
-    public string Size { get; set; }
     public MS Word() { }
 
     public MS Word(
@@ -18,13 +15,7 @@ namespace ConsoleApp{
       string [] Keywords,
       string Subjects,
       string Path,
-      string DateCreation,
-      string DateChange,
-      string Size
     ) : base(Name, Author, Keywords, Subjects, Path) {
-          this.DateCreation = DateCreation;
-          this.DateChange = DateChange;
-          this.Size = Size;
         }
 
         public MS Word (string Name, string Author, string[] Keywords, string Subjects, string Path)
@@ -37,32 +28,6 @@ namespace ConsoleApp{
 
           Console.Write("Тип файла: ");
           Console.WriteLine("DOC");
-
-          if (this.DateCreation == "") {
-            ValueParameter = "Не существует";
-          } else {
-              ValueParameter = this.DateCreation;
-            }
-
-            base.Print("Дата создания: ", ValueParameter);
-            ValueParameter = "";
-
-            if (this.DateChange == "") {
-                ValueParameter = "Не существует";
-            } else{
-                ValueParameter = this.DateChange;
-            }
-
-            base.Print("Дата изменения: ", ValueParameter);
-            ValueParameter = "";
-
-            if (this.Size == "") {
-              ParameterValue = "Не существует";
-            } else {
-                ParameterValue = this.Size;
-            }
-            base.Print("Размер файла: ", ParameterValue);
-            ParameterValue = "";
         }
     }
 }
