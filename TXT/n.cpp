03c4bd383ae16d6bdeae6a1e83eafe618d4f1dd1
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-02-20 00:23:20 +0700

    Update n.cpp

--- TXT/n.cpp
+++ TXT/n.cpp
@@ -7,30 +7,22 @@ using System.Reflection.Emit;
 
 namespace ConsoleApp{
   class TXT : LabTwo{
+    public string DateCreation { get; set; }
+    public string Language { get; set; }
     public TXT() { }
 
-    public TXT(
-      string Name,
-      string Author,
-      string[] Keywords,
-      string Subjects,
-      string Path,
-    ) : base(Name, Author, Keywords, Subjects, Path) {
-          this.DateOfCreation = DateOfCreation;
-          this.DateOfChange = DateOfChange;
-          this.Size = Size;
-        }
+    public TXT(string Name, string Author, string[] Keywords, string Subjects, string Path)
+      : base(Name, Author, Keywords, Subjects, Path) { }
 
-        public TXT(string Name, string Author, string[] Keywords, string Subjects, string Path)
-          : base(Name, Author, Keywords, Subjects, Path) { }
+    public TXT(string Name) : base(Name) { }
+    public override void ValueParameter() {
+      string ParameterValue = "";
 
-        public TXT(string Name) : base(Name) { }
-        public override void ValueParameter() {
-          string ParameterValue = "";
-
-          Console.Write("Тип файла: ");
-          Console.WriteLine("TXT");
-        }
+      Console.Write("Тип файла: ");
+      Console.WriteLine("TXT");
+      Console.WriteLine($"Дата создания: {DateCreation}\nЯзык: {Language}");
+      Console.ReadKey();
     }
+  }
 }
 
