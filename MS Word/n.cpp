787874d9b1ff9ec9777d28d4d0e42fc11eb1dadd
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-02-20 00:21:00 +0700

    Update n.cpp

--- MS Word/n.cpp
+++ MS Word/n.cpp
@@ -7,27 +7,23 @@ using System.Reflection.Emit;
 
 namespace ConsoleApp{
   class MS Word : LabTwo {
+    public string LetterSize { get; set; }
+    public int LetterFont { get; set; }
+    public string ElapsedTime { get; set; }
     public MS Word() { }
 
-    public MS Word(
-      string Name,
-      string Author,
-      string [] Keywords,
-      string Subjects,
-      string Path,
-    ) : base(Name, Author, Keywords, Subjects, Path) {
-        }
+    public MS Word (string Name, string Author, string[] Keywords, string Subjects, string Path)
+      : base (Name, Author, Keywords, Subjects, Path) { }
 
-        public MS Word (string Name, string Author, string[] Keywords, string Subjects, string Path)
-          : base (Name, Author, Keywords, Subjects, Path) { }
+    public MS Word(string Name) : base(Name) { }
 
-        public MS Word(string Name) : base(Name) { }
+    public override void PrintValues() { //override предоставляет новую реализацию метода, унаследованного от базового класса
+      string ParameterValue = "";
 
-        public override void PrintValues() { //override предоставляет новую реализацию метода, унаследованного от базового класса
-          string ParameterValue = "";
-
-          Console.Write("Тип файла: ");
-          Console.WriteLine("DOC");
-        }
+       Console.Write("Тип файла: ");
+       Console.WriteLine("DOC");
+       Console.WriteLine($"Размер букв: {LetterSize}\nШрифт букв: {LetterFont}\nЗатраченное время: {ElapsedTime}");
+       Console.ReadKey();
     }
+  }
 }
