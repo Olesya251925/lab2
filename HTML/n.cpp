3166093f1034b54a83ff878d72b5bbe6b08b562b
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-02-20 00:19:37 +0700

    Update n.cpp

--- HTML/n.cpp
+++ HTML/n.cpp
@@ -11,26 +11,17 @@ namespace ConsoleApp{
     public string ElapsedTime { get; set; }
     public HTML() { }
 
-    public HTML(
-      string Name,
-      string Author,
-      string [] Keywords,
-      string Subjects,
-      string Path,
-    ) : base(Name, Author, Keywords, Subjects, Path){ //base используется для доступа к членам базового класса из производного класса
-        }
+    public HTML(string Name, string Author, string[] Keywords, string Subjects, string Path)
+      : base(Name, Author, Keywords, Subjects, Path) { }
 
-        public HTML(string Name, string Author, string[] Keywords, string Subjects, string Path)
-        : base(Name, Author, Keywords, Subjects, Path) { }
+      public HTML(string Name) : base(Name) { } 
 
-        public HTML(string Name) : base(Name) { } 
-
-        public override void PrintValues(){
-          string ValueParameter = "";
-          Console.Write("Тип файла: ");
-          Console.WriteLine("HTML");
-          Console.WriteLine($"Дата создания: {DateCreation}\nЗатраченное время: {ElapsedTime}");
-          Console.ReadKey();
-        }
-    }
+      public override void PrintValues(){
+        string ValueParameter = "";
+        Console.Write("Тип файла: ");
+        Console.WriteLine("HTML");
+        Console.WriteLine($"Дата создания: {DateCreation}\nЗатраченное время: {ElapsedTime}");
+        Console.ReadKey();
+      }
+   }
 }
