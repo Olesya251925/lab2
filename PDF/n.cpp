6d567d865bf81d3612bc82a98f2cfec33f72f344
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-02-20 00:22:12 +0700

    Update n.cpp

--- PDF/n.cpp
+++ PDF/n.cpp
@@ -7,24 +7,22 @@ using System.Reflection.Emit;
 
 namespace ConsoleApp{
   class PDF : LabTwo {
+    public string Title { get; set; }
+    public string NumberPages { get; set; }
     public PDF() { }
 
-    public PDF(
-      string Name,
-      string Author,
-      string [] Keywords,
-      string Subjects,
-      string Path,
-    ) : base(Name, Author, Keywords, Subjects, Path){
-        }
+    public PDF (string Name, string Author, string[] Keywords, string Subjects, string Path)
+      : base (Name, Author, Keywords, Subjects, Path) { }
 
-	public PDF(string Name) : base(Name) { }
-        public override void PrintValues() {
-	  string ParameterValue = "";
+    public PDF(string Name) : base(Name) { }
+    
+    public override void PrintValues() {
+      string ParameterValue = "";
 
-	  Console.Write("Тип файла: ");
-	  Console.WriteLine("PDF");
-          base.ValueParameter();
-        }
+      Console.Write("Тип файла: ");
+      Console.WriteLine("PDF");
+      Console.WriteLine($"Заголовок: {Title}\nКоличество страниц: {NumberPages}");
+      Console.ReadKey();
     }
+  }
 }
