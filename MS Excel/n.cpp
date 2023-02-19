04c7d531fce9256600bc1308ef20b491db458118
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-02-19 23:23:37 +0700

    Update n.cpp

--- MS Excel/n.cpp
+++ MS Excel/n.cpp
@@ -7,9 +7,6 @@ using System.Reflection.Emit;
 
 namespace ConsoleApp{
   class MS Excel : LabTwo {
-    public string DateCreation { get; set; }
-    public string DateChange { get; set; }
-    public string Size { get; set; }
     public Excel() { }
 
     public MS Excel(
@@ -17,14 +14,8 @@ namespace ConsoleApp{
       string Author,
       string [] Keywords,
       string Subjects,
-      string Path,
-      string DateCreation,
-      string DateChange,
-      string Size
+      string Path
     ) : base(Name, Author, Keywords, Subjects, Path){
-          this.DateOfCreation = DateCreation;
-          this.DateOfChange = DateChange;
-          this.Size = Size;
         }
 
         public MS Excel(string Name, string Author, string[] Keywords, string Subjects, string Path)
@@ -34,35 +25,9 @@ namespace ConsoleApp{
 
         public override void ValueParameter() {
           string ValueParameter = "";
-          
+
           Console.Write("Тип файла: ");
           Console.WriteLine("MS Excel");
-
-          if (this.DateCreation == "") {
-            ValueParameter = "Не существует";
-          } else {
-              ValueParameter = this.DateCreation;
-            }
-
-            base.Print("Дата создания: ", ValueParameter);
-            ValueParameter = "";
-
-            if (this.DateChange == "") {
-                ValueParameter = "Не существует";
-            } else{
-                ValueParameter = this.DateChange;
-            }
-
-            base.Print("Дата изменения: ", ValueParameter);
-            ValueParameter = "";
-
-            if (this.Size == "") {
-              ParameterValue = "Не существует";
-            } else {
-                ParameterValue = this.Size;
-            }
-            base.Print("Размер файла: ", ParameterValue);
-            ParameterValue = "";
         }
     }
 }
