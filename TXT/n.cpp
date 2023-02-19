f9ba02424e24e7ae92b6973b004d0115ec4e56bf
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-02-19 23:26:55 +0700

    Update n.cpp

--- TXT/n.cpp
+++ TXT/n.cpp
@@ -6,11 +6,7 @@ using System;
 using System.Reflection.Emit;
 
 namespace ConsoleApp{
-  class TXT : LabTwo {
-    public string DateCreation { get; set; }
-    public string DateChange { get; set; }
-    public string Size { get; set; }
-
+  class TXT : LabTwo{
     public TXT() { }
 
     public TXT(
@@ -19,9 +15,6 @@ namespace ConsoleApp{
       string[] Keywords,
       string Subjects,
       string Path,
-      string DateCreation,
-      string DateChange,
-      string Size
     ) : base(Name, Author, Keywords, Subjects, Path) {
           this.DateOfCreation = DateOfCreation;
           this.DateOfChange = DateOfChange;
@@ -37,32 +30,7 @@ namespace ConsoleApp{
 
           Console.Write("Тип файла: ");
           Console.WriteLine("TXT");
-
-          if (this.DateCreation == ""){
-            ValueParameter = "Не существует";
-          } else {
-            ValueParameter = this.DateCreation;
-            }
-
-            base.Print("Дата создания: ", ValueParameter);
-            ValueParameter = "";
-
-            if (this.DateChange == "") {
-              ValueParameter = "Не существует";
-            } else {
-                ValueParameter = this.DateChange;
-            }
-
-            base.Print("Дата изменения: ", ValueParameter);
-            ValueParameter = "";
-
-            if (this.Size == "") {
-              ValueParameter = "Не существует";
-            } else {
-                ValueParameter = this.Size;
-            }
-            base.Print("Размер файла: ", ValueParameter);
-            ValueParameter = "";
         }
     }
 }
+
