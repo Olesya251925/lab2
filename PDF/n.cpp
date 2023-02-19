5b2d286fe2c3484b264038544d6f99b4d350ca33
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-02-19 23:25:33 +0700

    Update n.cpp

--- PDF/n.cpp
+++ PDF/n.cpp
@@ -7,9 +7,6 @@ using System.Reflection.Emit;
 
 namespace ConsoleApp{
   class PDF : LabTwo {
-    public string DateCreation { get; set; }
-    public string DateChange { get; set; }
-    public string Size { get; set; }
     public PDF() { }
 
     public PDF(
@@ -18,49 +15,16 @@ namespace ConsoleApp{
       string [] Keywords,
       string Subjects,
       string Path,
-      string DateCreation,
-      string DateChange,
-      string Size
     ) : base(Name, Author, Keywords, Subjects, Path){
-          this.DateOfCreation = DateCreation;
-          this.DateOfChange = DateChange;
-          this.Size = Size;
         }
-		
+
 	public PDF(string Name) : base(Name) { }
         public override void PrintValues() {
 	  string ParameterValue = "";
 
-	 Console.Write("Тип файла: ");
-	 Console.WriteLine("PDF");
-         base.ValueParameter();
-
-	if (this.DateCreation == "") {
-          ValueParameter = "Не существует";
-        } else {
-            ValueParameter = this.DateCreation;
-          }
-
-          base.Print("Дата создания: ", ValueParameter);
-          ValueParameter = "";
-
-          if (this.DateChange == "") {
-            ValueParameter = "Не существует";
-          } else{
-            ValueParameter = this.DateChange;
-          }
-
-          base.Print("Дата изменения: ", ValueParameter);
-          ValueParameter = "";
-
-          if (this.Size == "") {
-            ParameterValue = "Не существует";
-          } else {
-              ParameterValue = this.Size;
-            }
-            
-            base.Print("Размер файла: ", ParameterValue);
-            ParameterValue = "";
+	  Console.Write("Тип файла: ");
+	  Console.WriteLine("PDF");
+          base.ValueParameter();
         }
     }
 }
