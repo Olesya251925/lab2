96f6033908982c35412498d0f0ddff68d2ae89db
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-02-20 13:20:36 +0700

    Update n.cpp

--- LabTwo/n.cpp
+++ LabTwo/n.cpp
@@ -8,12 +8,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace LabTwo{
-  public static class Singleton {
+  public static class Singleton Instance {
+   get {
+     if (instance == null) instance = new Singleton();
+     return instance;
+    }
+   } 
     public string Name { get; set; }
     public string Author { get; set; }
     public string [] Keywords { get; set; }
     public string Subjects { get; set; }
     public string Path { get; set; }
+   
+    private Singleton() { }
+    private static Singleton instance;
 
     public Singleton (string name, string author, string[] keywords, string subjects, string path){
       Namt = name;
