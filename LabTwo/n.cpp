1f37065c5a1925237192781fb74d1f3b2c51f8f5
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-02-20 16:58:18 +0700

    Update n.cpp

--- LabTwo/n.cpp
+++ LabTwo/n.cpp
@@ -7,29 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LabTwo{
-  public static class Singleton Instance {
-   get {
-     if (instance == null) instance = new Singleton();
-     return instance;
-    }
-   } 
+namespace LabTwo {
+  public abstract class ElDocum {
+
     public string Name { get; set; }
     public string Author { get; set; }
     public string [] Keywords { get; set; }
     public string Subjects { get; set; }
     public string Path { get; set; }
-   
-    private Singleton() { }
-    private static Singleton instance;
-
-    public Singleton (string name, string author, string[] keywords, string subjects, string path){
+  
+    public ElDocum (string name, string author, string[] keywords, string subjects, string path){
       Namt = name;
       Author = author;
       Keywords = keywords;
       Subjects = subjects;
       Path = path;
     }
-    public static class OutputLines();
+    public abstract string OutputLines();
   }
 }
