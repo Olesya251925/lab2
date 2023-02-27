9a312dc041ca14e92a24e3cdeb059520e8025944
Olesya251925 <118814113+Olesya251925@users.noreply.github.com>
2023-02-27 12:32:56 +0700

    Update n.cpp

--- ElDocum/n.cpp
+++ ElDocum/n.cpp
@@ -3,101 +3,94 @@
 ***************************/
 using System;
  namespace LabTwo {
-   public sealed class OutputInfo {
-     private static readonly OutputInfo instance = new OutputInfo();
-       private List<ElDocum > documents = new List<ElDocum>();
+   public class Singleton {
+    public static Singleton Instance {
+      get {
+        if (instance == null) instance = new Singleton();
+        return instance;
+      }
+    }
+    public void MenuVariants() {
+      Console.WriteLine("1 - Новый документ");
+      Console.WriteLine("2 - Все документы");
+      Console.WriteLine("3 - Выйти");
 
-       private OutputInfo() { }
+      string choice = Console.ReadLine();
+        switch (choice) {
+          case "1":
+            DocumentMenu();
+            break;
+          case "2":
+            ListAllDocuments();
+            break;
+          case "3":
+            Environment.Exit(0);
+            break;
+          default:
+          Console.WriteLine("Ошибка. Документ не существует");
+            break;
+        }
+        MenuVariants();
+      }
 
-       public static OutputInfo Instance {
-         get { return instance; }
-       }
+      private void DocumentMenu() {
+      ElDocum Information = new ElDocum {
+        Name = "Игры, в которые играют люди",
+        Authors = "Эрик Берн",
+        Subject = "Психология",
+        PathToFaile = "https://www.litres.ru/erik",
+        KeyWords = new string[] { "Игры; ", "Люди; "},
+      };
+      DocumentOutput(Information);
 
-       public void DobDocument(ElDocum doc) {
-         documents.Dob(doc);
-       }
+      Console.WriteLine("Введите документ, которым интересуетесь: ");
+      Console.WriteLine("1. MS Word\n2. PDF\n3. MS Excel\n4. TXT\n5. HTML");
+      string choice = Console.ReadLine();
 
-       public void MenuVariants() {
-         Console.WriteLine("1 - Новый документ");
-         Console.WriteLine("2 - Все документы");
-         Console.WriteLine("3 - Выйти");
-
-         string choice = Console.ReadLine();
-         switch (choice) {
-           case "1":
-             DocumentMenu();
-             break;
-           case "2":
-             ListAllDocuments();
-             break;
-           case "3":
-             Environment.Exit(0);
-             break;
-           default:
-             Console.WriteLine("Ошибка. Документ не существует");
-              break;
-         }
-         MenuVariants();
-       }
-
-       private void DocumentMenu() {
-       ElDocum Information = new ElDocum {
-         Name = "Игры, в которые играют люди",
-         Authors = "Эрик Берн",
-         Subject = "Психология",
-         PathToFaile = "https://www.litres.ru/erik",
-         KeyWords = new string[] { "Игры; ", "Люди; "},
-       };
-       DocumentOutput(Information);
-
-       Console.WriteLine("Введите документ, которым интересуетесь: ");
-       Console.WriteLine("1. MS Word\n2. PDF\n3. MS Excel\n4. TXT\n5. HTML");
-       string choice = Console.ReadLine();
-
-       switch (choice) {
-         case "1":
-          ElDocum InformationWord = new Word {
-            LetterSize = 14,
-            LetterFont = "Times New Roman",
-            ElapsedTime = 254,
-         };
-         DocumentOutput(InformationWord);
-         break;
+      switch (choice) {
+        case "1":
+        ElDocum InformationWord = new Word {
+          LetterSize = 14,
+          LetterFont = "Times New Roman",
+          ElapsedTime = 254,
+        };
+        DocumentOutput(InformationWord);
+        break;
          
-         case "2":
-         ElDocum InformationPDF = new PDF {
-           Title = "Компьютер",
-           NumberPages = 56,
-         };
-         DocumentOutput(InformationPDF);
-         break;
+        case "2":
+        ElDocum InformationPDF = new PDF {
+          Title = "Компьютер",
+          NumberPages = 56,
+        };
+        DocumentOutput(InformationPDF);
+        break;
          
-         case "3":
-         ElDocum InformationExcel = new Excel {
-           NumberColumns = 6,
-           NumberRows = 9,
-         };
-         DocumentOutput(InformationExcel);
-         break;
+        case "3":
+        ElDocum InformationExcel = new Excel {
+          NumberColumns = 6,
+          NumberRows = 9,
+        };
+        DocumentOutput(InformationExcel);
+        break;
                  
-         case "4":
-         ElDocum InformationTXT = new TXT {
-           DateCreation = "15.09.2018",
-           Language = "Русский",
-         };
-         DocumentOutput(InformationTXT);
-         break;
+        case "4":
+        ElDocum InformationTXT = new TXT {
+          DateCreation = "15.09.2018",
+          Language = "Русский",
+        };
+        DocumentOutput(InformationTXT);
+        break;
          
-         case "5":
-         ElDocum InformationHTML = new HTML {
-           DateCreation = "18.02.2020",
-           ElapsedTime = 156,
-         };
-         DocumentOutput(InformationHTML);
-         break;
-         default:
-         Console.WriteLine("Данный документ не существует");
-         break;
+        case "5":
+        ElDocum InformationHTML = new HTML {
+          DateCreation = "18.02.2020",
+          ElapsedTime = 156,
+        };
+        DocumentOutput(InformationHTML);
+        break;
+        default:
+        Console.WriteLine("Данный документ не существует");
+        break;
         }
       }
       private void ListAllDocuments() {
@@ -108,11 +101,13 @@ using System;
         }
         Console.WriteLine();
       }
+      private Singleton(){}
+      private static Singleton instance
     }
-    class OutputInformation {
+    internal class Program {
       static void Main(string[] args) {
-        OutputInfo outInfo = OutputInfo.Instance;
-        outInfo.MenuVariants();
+        Singleton.Instance.Method();
+        Console.ReadKey();
       }
    }
 }
